5df22a1fc6a9c4f44c2b674c8cf7e284c3c2ead9
miguelanggelo <miguelangelcardenasgomez@gmail.com>
2021-07-02 07:24:52 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');    
+}) // aparece y desaparece el sidebar//
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 500} );
+ScrollReveal().reveal('.card-banner-one', {delay: 500} );
+ScrollReveal().reveal('.card-banner-two', {delay: 500} );
+
+
